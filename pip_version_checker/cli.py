615526dee4e0208d7478109eae1747ea7264a836
Nayden Naydev <nayden.naydev@de-cix.net>
2021-12-10 11:29:38 +0100

    Add --fail option
    Utilize clock.echo as well

--- pip_version_checker/cli.py
+++ pip_version_checker/cli.py
@@ -8,12 +8,16 @@ from pip_version_checker.pip_version import parse_requirements_file
 @click.option(
     "--filename", default="requirements.txt", help="File with required python packages"
 )
-def main(filename):
+@click.option(
+    "--fail", type=bool, default=False, help="Fail if there are outdated packages"
+)
+def main(filename, fail):
     requirements_in_file = parse_requirements_file(filename)
     outdated_packages = [r for r in requirements_in_file if r.outdated]
     if outdated_packages:
-        print(f"Outdated packages in {filename}:", stdout)
+        click.echo(f"Outdated packages in {filename}:", file=stdout)
     else:
-        print(f"No outdated packages found in {filename}", stdout)
+        click.echo(f"No outdated packages found in {filename}", file=stdout)
     for op in outdated_packages:
-        print(op)
+        click.echo(op)
+    exit(fail and bool(outdated_packages))
